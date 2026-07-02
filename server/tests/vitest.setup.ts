1a7bd85ce32205de505172f189f23c5c357e06b3
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-07-02 15:13:01 +0000

    setup basic vitest config

---
+++ server/tests/vitest.setup.ts
@@ -0,0 +1,36 @@
+import { execSync } from "child_process";
+import { PrismaClient } from "@prisma/client";
+
+const { POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
+const TEST_DB = "app_test";
+const DATABASE_URL_TEST = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/${TEST_DB}`;
+const DATABASE_URL_ADMIN = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/postgres`;
+
+export async function setup() {
+  const adminClient = new PrismaClient({
+    datasources: { db: { url: DATABASE_URL_ADMIN } },
+  });
+
+  await adminClient.$executeRawUnsafe(`DROP DATABASE IF EXISTS ${TEST_DB};`);
+  await adminClient.$executeRawUnsafe(`CREATE DATABASE ${TEST_DB};`);
+  await adminClient.$disconnect();
+
+  execSync("npm run prisma:migrate:deploy", {
+    env: {
+      ...process.env,
+      DATABASE_URL: DATABASE_URL_TEST,
+    },
+  });
+
+  process.env.DATABASE_URL = DATABASE_URL_TEST;
+}
+
+export async function teardown() {
+  const adminClient = new PrismaClient({
+    datasources: { db: { url: DATABASE_URL_ADMIN } },
+  });
+
+  await adminClient.$executeRawUnsafe(`DROP DATABASE IF EXISTS ${TEST_DB};`);
+  console.log(`database ${TEST_DB} deleted`);
+  await adminClient.$disconnect();
+}
