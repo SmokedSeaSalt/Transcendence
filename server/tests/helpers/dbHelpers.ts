8d7988cdf4a03e4bd839a5a64e089c5e066c8a86
edsardgrisel <edsardgrisel1@gmail.com>
2026-07-03 12:43:45 +0000

    tests for userRegistratiom created for api

---
+++ server/tests/helpers/dbHelpers.ts
@@ -0,0 +1,8 @@
+// import { PrismaClient } from "@prisma/client";
+
+// export async function deleteUser(email: string) {
+// 	await prisma.$transaction([
+// 		prisma.user.deleteMany(),
+// 		prisma.apiKey.deleteMany(),
+// 	]);
+// }
--- server/tests/vitest.setup.ts
+++ server/tests/vitest.setup.ts
@@ -6,14 +6,19 @@ const TEST_DB = "app_test";
 const DATABASE_URL_TEST = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/${TEST_DB}`;
 const DATABASE_URL_ADMIN = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/postgres`;
 
+const adminClient = new PrismaClient({
+  datasources: { db: { url: DATABASE_URL_ADMIN } },
+});
+
+export const testClient = new PrismaClient({
+    datasources: { db: { url: DATABASE_URL_TEST } },
+});
+
 export async function setup() {
-  const adminClient = new PrismaClient({
-    datasources: { db: { url: DATABASE_URL_ADMIN } },
-  });
 
   await adminClient.$executeRawUnsafe(`DROP DATABASE IF EXISTS ${TEST_DB};`);
   await adminClient.$executeRawUnsafe(`CREATE DATABASE ${TEST_DB};`);
-  await adminClient.$disconnect();
+  // await adminClient.$disconnect();
 
   execSync("npm run prisma:migrate:deploy", {
     env: {
@@ -26,9 +31,9 @@ export async function setup() {
 }
 
 export async function teardown() {
-  const adminClient = new PrismaClient({
-    datasources: { db: { url: DATABASE_URL_ADMIN } },
-  });
+  // const adminClient = new PrismaClient({
+  //   datasources: { db: { url: DATABASE_URL_ADMIN } },
+  // });
 
   await adminClient.$executeRawUnsafe(`DROP DATABASE IF EXISTS ${TEST_DB};`);
   console.log(`database ${TEST_DB} deleted`);
