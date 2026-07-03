76966368b76e8326fc75138489a1805bc4dd815d
edsardgrisel <edsardgrisel1@gmail.com>
2026-07-03 13:26:54 +0000

    extra valid cases added userRegistration

--- server/tests/integration/integration.test.ts
+++ server/tests/integration/integration.test.ts
@@ -8,6 +8,12 @@ const registerPaths = [
 	"/web/users/register",
 ] as const;
 
+const validCases = [
+	{email: "abc@gmail.com", name: "john", password: "ValidPassword123!"},
+	{email: "helloworld@hotmail.net", name: "hello world abcdef", password: "!!!!!Aa1"},
+	{email: "mat@yahoo.co", name: "m", password: "superlong_ValidPassword?123456!"},
+];
+
 const invalidEmails = [
 	"abc",
 	"google.com",
@@ -31,10 +37,14 @@ const invalidNames = [
 	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
 ];
 
+const deleteValidCaseUser = async () => {
+	for (const { email } of validCases) {
+		await testClient.user.deleteMany({ where: { email } });
+	}
+}
+
 const deleteUser = async (email: string) => {
-	await testClient.user.deleteMany({
-		where: { email }
-	});
+	await testClient.user.deleteMany({ where: { email } });
 }
 
 const createUser = async (email: string, name: string, password: string) => {
@@ -46,6 +56,11 @@ const createUser = async (email: string, name: string, password: string) => {
 			apiKey: "verySecureKey"
 		}
 	});
+}
+
+const userExists = async (email: string) => {
+	const user = await testClient.user.findUnique({ where: { email } });
+	return user ? true : false;
 }
 
 describe("GET /health", () => {
@@ -62,10 +77,10 @@ describe.each(registerPaths)("POST %s", (path) => {
 	const password = "ValidPassword123!";
 
 	beforeEach(async () => {
-		await deleteUser(email);
+		await deleteValidCaseUser();
 	});
 
-	it("returns 201 created", async () => {
+	it.each(validCases)("returns 201 created $email", async ({email, name, password}) => {
 		const res = await request(app)
 			.post(path)
 			.send({ email, name, password });
@@ -73,6 +88,8 @@ describe.each(registerPaths)("POST %s", (path) => {
 		expect(res.status).toBe(201);
 		expect(res.body.email).toBe(email);
 		expect(res.body.name).toBe(name);
+
+		expect(userExists, `User with ${email} has not been added to the database.`).toBeTruthy();
 	});
 
 	it("returns 409 conflict", async () => {
@@ -83,6 +100,7 @@ describe.each(registerPaths)("POST %s", (path) => {
 			.send({ email, name, password });
 
 		expect(res.status).toBe(409);
+		await deleteUser(email);
 	});
 
 	it.each(invalidEmails)("returns 400 invalid email: %s", async (badEmail) => {
@@ -110,5 +128,9 @@ describe.each(registerPaths)("POST %s", (path) => {
 
 		expect(res.status).toBe(400);
 		expect(res.body.errors[0].msg).not.toHaveLength(0);
+	});
+
+	afterAll(async () => {
+		await deleteValidCaseUser();
 	});
 });
