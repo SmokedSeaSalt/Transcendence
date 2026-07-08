d30305e6791349e9cb29c00abdb16396e97858ab
edsardgrisel <edsardgrisel1@gmail.com>
2026-07-08 14:11:25 +0000

    add tos and privacy policy.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CallHealth from "./pages/CallHealthy";
 import Login from "./pages/Login";
+import TermsOfService from  "./pages/TermsOfService"
+import PrivacyPolicy from "./pages/PrivacyPolicy"
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/login" element={<Login />} />
+				<Route path="/terms-of-service" element={<TermsOfService />} />
+				<Route path="/privacy-policy" element={<PrivacyPolicy />} />
+
 				<Route path="/" element={<CallHealth />} />
 			</Routes>
 		</BrowserRouter>
