0b440079f353f8ad14d68a37c341965e33b57469
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-07-10 16:06:24 +0200

    update test framework to override prisma client to the test database one

--- server/tests/vitest.config.ts
+++ server/tests/vitest.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	test: {
-		globalSetup: "./tests/vitest.setup.ts",
-	},
+  test: {
+    globalSetup: "./tests/vitest.global.setup.ts",
+    setupFiles: "./tests/vitest.setup.ts",
+  },
 });
