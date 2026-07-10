9f41d457eede048d5d4a344468e55ec8073fa71b
Manon <mgroos@student.codam.nl>
2026-07-10 16:42:14 +0200

    buttons; code formatting; mechanics are there to use a logged_in variable, but no access to it yet

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,31 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo_temp_cat.png"
+import logo from "../assets/logo_temp_cat.png";
 
 export default function Header() {
+	const is_logged_in = true; /* replace with actual login status from somewhere else*/
+
 	return (
-		<header className="bg-white border-t border-gray-200 mt-8">
-			<div className="max-w-7xl mx-auto px-4 py-6 flex flex-col sm:flex-row items-center justify-between text-sm text-gray-600">
-				{/* make image clickable to home page? */}
-				<div>
-				<Link to="/">
+		<header className="bg-gray-900 border-t mt-0">
+			<div className="max-w-7xl mx-auto px-4 py-6 flex flex-col sm:flex-row items-center justify-between text-sm text-orange-600">
+				<div className="left-30">
+					<Link to="/">
 						<img src={logo} alt="typeracer logo" width="120" height="120" />
-				</Link>
+					</Link>
 				</div>
-				<div><h1>Game Name</h1></div>
+				<div className="text-xl font-bold">
+					<h1>Transcendence Typeracer</h1>
+				</div>
 				<div className="mt-3 sm:mt-0 flex items-center space-x-4">
-					<Link
-						to="/leaderboard"
-						className="text-gray-700 hover:text-blue-600"
-					>
-						🏆 Leaderboard
+					<Link to="/leaderboard">
+						<button
+							type="button"
+							className="bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded-xl"
+						>
+							🏆 Leaderboard
+						</button>
 					</Link>
-					<Link
-						to="/profile"
-						className="text-gray-700 hover:text-blue-600"
-					>
-						[Log in / Make an account OR view profile]
-					</Link>
+					{is_logged_in ? (
+						<Link to="/profile" className="text-white hover:text-orange-600">
+							<button
+								type="button"
+								className="bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded-xl"
+							>
+								View profile
+							</button>
+						</Link>
+					) : (
+						<Link to="/login" className="text-white hover:text-orange-600">
+							<button
+								type="button"
+								className="bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded-xl"
+							>
+								Log in / Make an Account
+							</button>
+						</Link>
+					)}
 				</div>
 			</div>
 		</header>
