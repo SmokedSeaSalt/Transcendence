0bea7d7f9e650d6effa835e23917657d01c991f5
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-07-15 22:59:09 +0200

    adapt context to fit new userauth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,64 +2,46 @@ import { Children, createContext, useContext, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { userAuth, type jsonUser } from "./hooks/userAuth";
+import { type jsonUser, userAuth } from "./hooks/userAuth";
 import CallHealth from "./pages/CallHealthy";
 import Login from "./pages/Login";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import Profile from "./pages/Profile";
 import TermsOfService from "./pages/TermsOfService";
 
+//create context type to mathc return object of userAuth
 export interface authContextType {
 	currentUser: jsonUser | null;
-	loggedIn: boolean;
-	setCurrentUser: (currentUser: jsonUser | null) => void;
-	refresh() : void;
-	setLoggedIn : (loggedIn: boolean) => void;
+	loading: boolean;
+	updateLoggedinUser: () => Promise<void>;
 }
 
-export const AuthContext = createContext<authContextType | null>({
-	currentUser: {name: "default name", email: "email addr", createdAt: "date"},
-	loggedIn: false,
-	setCurrentUser: () => {},
-	refresh: () => {},
-	setLoggedIn: () => {},
-});
+export const AuthContext = createContext<authContextType | null>(null);
 
 // provides functions for the AuthContext
-export const AuthProvider = ({ children }: {children: React.ReactNode}) => {
-	const [ userInfo, loading ] = userAuth();
-	var [currentUser, setCurrentUser] = useState<jsonUser | null>(userInfo);
-	var [loggedIn, setLoggedIn] = useState<boolean>(false);
-	// todo: wait for loading to not be null ? -> doesn't seem to have any influence
-	if (loading)
-		console.log("In authprovider, loading is true.");
-	console.log("pre-refresh, user without setting is:", userInfo);
-	// setCurrentUser(userInfo); // not allowed, too many renders
-	currentUser = userInfo;
-	if (userInfo !== null)
-		loggedIn = true;
-	const refresh = () => {
-		setCurrentUser(userInfo);
-		console.log("now running refresh, setting user to :", userInfo);
-	}
-	return (
-		<AuthContext value={{currentUser, loggedIn, setCurrentUser, refresh, setLoggedIn}}>
-			{children}
-		</AuthContext>
-	)
-}
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+	//deconstruct returned userAuth object.
+	const { userData, loading, refetch } = userAuth();
 
+	//create object to get all this info from the same context value
+	const value: authContextType = {
+		currentUser: userData,
+		loading,
+		updateLoggedinUser: refetch,
+	};
+
+	return <AuthContext value={value}>{children}</AuthContext>;
+};
+
+//helper wrapper needed for typescript
 export const useAuthContext = () => {
 	const context = useContext(AuthContext);
-	console.log("useAuthContext being used.");
-	if (!context)
-	{
-		console.log("error useAuthContext");
+	if (!context) {
 		throw new Error("useAuthContext used inappropriately");
 		// todo: catch somewhere?
 	}
 	return context;
-}
+};
 
 export default function App() {
 	return (
