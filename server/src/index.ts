65e727998ff3a06555a72196b5a3c924b2c9a236
edsardgrisel <edsardgrisel1@gmail.com>
2026-07-16 15:12:27 +0200

    unused import removed

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
 import { app } from "./app.js";
 import { createAdminUser } from "./config/dbSeed.js";
-import { updateAPIKey } from "./services/apiKeyServices.js";
 
 const port = Number(process.env.PORT ?? 8000);
 
