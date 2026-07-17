e38837aa57c989d32e22f7268f3cc1944f81befc
edsardgrisel <edsardgrisel1@gmail.com>
2026-07-17 16:21:03 +0200

    api auth middleware tests done. can delete other api key related fail test cases from other test files

---
+++ server/tests/integration/auth/integration_requireAdmin.test.ts
@@ -0,0 +1,57 @@
+import { createHash } from "node:crypto";
+import request from "supertest";
+import { afterEach, describe, expect, it } from "vitest";
+import { app } from "../../../src/app.js";
+import {
+	createApiKey,
+	createUser,
+	createUserWithRoleAndApiKey,
+	deleteUser,
+} from "../../helpers/dbHelpers.js";
+
+
+describe("requireAdmin middleware", () => {
+	const registerUserRoute = "/api/users/register"
+
+
+	const email = "testAdmin@example.com";
+	const name = "Test Admin User";
+	const password = "ValidPassword123!";
+	const unhashedApiKey = "key";
+
+	it("rejects non-admin users", async () => {
+		await createUserWithRoleAndApiKey(
+			email,
+			name,
+			password,
+			unhashedApiKey,
+			"user",
+		);
+		const res = await request(app)
+			.delete(registerUserRoute)
+			.set("Authorization", unhashedApiKey);
+
+		expect(res.status).toBe(403);
+		expect(res.text).toContain("Admin access required");
+	});
+
+	it("allows admin users", async () => {
+		await createUserWithRoleAndApiKey(
+			email,
+			name,
+			password,
+			unhashedApiKey,
+			"admin",
+		);
+		const res = await request(app)
+			.post(registerUserRoute)
+			.set("Authorization", unhashedApiKey)
+			.send({email: "new@gmail.com", name: "jon", password: "Password1!"});
+
+		expect(res.status).toBe(201);
+	});
+
+	afterEach(async () => {
+		await deleteUser(email);
+	});
+});
