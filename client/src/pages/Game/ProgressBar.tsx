6e48d418ab7b1e14b996f7e14c3df2cd4fd99452
Manon <mgroos@student.codam.nl>
2026-07-22 13:44:11 +0200

    marker position can be changed by variable

--- client/src/pages/Game/ProgressBar.tsx
+++ client/src/pages/Game/ProgressBar.tsx
@@ -1,24 +1,39 @@
 import React from "react";
+import type * as CSS from 'csstype';
 
 export default function ProgressBar() {
-	var total_words = 100;
+	var total_words = 90;
 	var current_words = 30;
-	// since position is not counted from object center, max distance is 97.5%
-	var progress: number = Math.round(current_words / total_words * 97.5);
-	var progressPercentage: string = "left-[" + progress + "%]"; // for some reason only certain percentages work
-	console.log("prog percentage: ", progressPercentage);
+	// todo: 97.9 depends on marker height/width (which depends on container height) and length set outside function
+	var progress: number = current_words / total_words * 98;
+
+	console.log("progress:", progress);
 	var barColour = "bg-blue-400";
-	var circleColour = "bg-blue-700";
+	var circleColour = "#2561c9";
+
+	// automatically updates distance from top when markerSize is updated so it's always centered
+	const markerSize = 80;
+	const topDistance = (100 - markerSize) / 2;
+	var markerStyle: CSS.Properties = {
+		height: `${markerSize}%`,
+		aspectRatio: 1/1,
+		position: `absolute`,
+		backgroundColor: `${circleColour}`,
+		left: `${progress}%`,
+		top: `${topDistance}%`,
+		borderRadius: '100%',
+	};
+
+	const containerStyle = "relative bottom-0 left-0 h-6 content-center";
+	// const containerStyle = "relative bg-gray-300 bottom-0 left-0 h-6 content-center";
+	const barStyle = `${barColour} h-2 rounded-sm left-0 bottom-0`;
 
 
 	return (
-		<div className="relative bottom-0 left-0 h-6 bg-gray-300 content-center">
-		{/* // <div className="relative h-6 bottom-0 left-0 content-center"> */}
-			<div className={`${barColour} h-2 rounded-sm left-0 bottom-0`}>
-				<div className={`relative bottom-[75%] ${progressPercentage} ${circleColour} h-5 w-5 rounded-full`}></div>
-				{/* <div className={`relative bottom-[75%] left-[26%] ${circleColour} h-5 w-5 rounded-full`}></div> */}
+		<div className={containerStyle}>
+			<div className={barStyle}>
+				<div style={markerStyle}></div>
 			</div>
 		</div>
 	);
 }
-
