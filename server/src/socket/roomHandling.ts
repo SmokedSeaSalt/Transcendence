18930f5ae26bff089cbc282fe8125693defa0011
edsardgrisel <edsardgrisel1@gmail.com>
2026-07-24 15:52:03 +0200

    incomming word validation functionality done.

--- server/src/socket/roomHandling.ts
+++ server/src/socket/roomHandling.ts
@@ -2,10 +2,12 @@ import type { Socket } from "socket.io";
 import { joinRoom } from "../services/roomService.js";
 
 export function registerRoomHandlers(socket: Socket) {
+	// todo get databaseUserId. socket.data.databaseUserId doesnt exist. change to socket.data.userId. also socket.data.name should be displayName
 	socket.on("joinRoom", (roomId: string) => {
 		if (
 			!joinRoom(roomId, socket.id, socket.data.name, socket.data.databaseUserId)
 		) {
+			// todo callback
 			console.log(`${socket.id} failed to joinRoom: ${roomId}`);
 		}
 		console.log(`${socket.id} joinRoom sucessful: ${roomId}`);
