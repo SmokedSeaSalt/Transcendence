29a94f00c7c8708d058129cd7aac1925c62f6910
edsardgrisel <edsardgrisel1@gmail.com>
2026-07-24 14:08:18 +0200

    format

--- server/src/socket/roomHandling.ts
+++ server/src/socket/roomHandling.ts
@@ -3,7 +3,9 @@ import { joinRoom } from "../services/roomService.js";
 
 export function registerRoomHandlers(socket: Socket) {
 	socket.on("joinRoom", (roomId: string) => {
-		if (!joinRoom(roomId, socket.id, socket.data.name, socket.data.databaseUserId)) {
+		if (
+			!joinRoom(roomId, socket.id, socket.data.name, socket.data.databaseUserId)
+		) {
 			console.log(`${socket.id} failed to joinRoom: ${roomId}`);
 		}
 		console.log(`${socket.id} joinRoom sucessful: ${roomId}`);
