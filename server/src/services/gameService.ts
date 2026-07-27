f901b198a86bf7af0978a100c9c632517628456b
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-07-27 11:59:53 +0200

    service to create new room with unique id

--- server/src/services/gameService.ts
+++ server/src/services/gameService.ts
@@ -1,1 +1,28 @@
 //validateIncommingWord(roomId, userId, typedWord);
+
+import { type RoomData, roomStore } from "./roomStore.js";
+
+//create a new roomstore room with unique id
+
+export const createUniqueRoom = (): RoomData | null => {
+	let roomId = makeid(6);
+	let tries = 0;
+	while (roomStore.get(roomId)) {
+		roomId = makeid(6);
+		if (tries > 100) return null;
+		tries++;
+	}
+	const room = roomStore.create(roomId);
+	return room;
+};
+
+//https://stackoverflow.com/questions/1349404/generate-a-string-of-random-characters
+function makeid(length: number) {
+	let result = "";
+	const characters = "0123456789";
+	const charactersLength = characters.length;
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+}
