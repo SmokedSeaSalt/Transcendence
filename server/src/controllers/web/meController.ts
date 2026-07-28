341d9418679efda254412587e517a6bfeaa26c6f
Racer911dash1 <douwe.bakker.03@gmail.com>
2026-07-28 11:43:38 +0200

    Move the function to a different file and implement a getGameResults function.

---
+++ server/src/controllers/web/meController.ts
@@ -0,0 +1,53 @@
+import { NextFunction, Request, Response } from "express";
+import { UnauthorizedError } from "../../errors/errorTypes.js";
+import { toPublicUser } from "../../dto/user.mapper.js";
+import * as userServices from "../../services/userServices.js";
+
+export const buildUserResponseFromSession = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		if (!req.user) {
+			return next(new UnauthorizedError("Unauthorized"));
+		}
+
+		const user = await userServices.getUserByID(req.user.id);
+		if (!user) {
+			return next(new UnauthorizedError("Unauthorized"));
+		}
+
+		const response = toPublicUser(user);
+
+		return res.status(200).json(response);
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			next(error);
+		} else {
+			next(new Error(String(error)));
+		}
+	}
+};
+
+export async function getGameResults(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		if (!req.user) {
+			return next(new UnauthorizedError("Unauthorized"));
+		}
+
+		const gameResults = await userServices.getAllGameResultsById(req.user.id);
+
+		return res.status(200).json(gameResults);
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			next(error);
+		} else {
+			next(new Error(String(error)));
+		}
+	}
+}
