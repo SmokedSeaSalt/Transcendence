63add61abdf1f3ee0bb6aa26d28cc3856b97dc8e
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-08-06 11:38:50 +0200

    logo changes

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
 			<div className="px-4 py-6 flex flex-col sm:flex-row items-center justify-between text-sm text-highlight-colored">
 				<div className="left-30">
 					<Link to="/">
-						<div className="bg-surface rounded-md p-2">
+						<div className="bg-background-secondary outline-2 rounded-md p-2">
 							<img src={logo} alt="typeracer logo" width="120" height="120" />
 						</div>
 					</Link>
