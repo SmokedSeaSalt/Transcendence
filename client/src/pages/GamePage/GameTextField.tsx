0cdc95fdcee248f9eb997196b7c76cedc5f89236
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-08-06 10:25:20 +0200

    bold wordcomlete and wrongtyped

--- client/src/pages/GamePage/GameTextField.tsx
+++ client/src/pages/GamePage/GameTextField.tsx
@@ -130,14 +130,16 @@ const GameTextField: React.FC<TextFieldProps> = (props) => {
 			) : (
 				<div className={outerClassName}>
 					<div className="text-xl font-mono box-border ">
-						<span className="text-typebox-completed inline">
+						<span className="text-typebox-completed font-bold inline">
 							{promptComplete}
 						</span>
 						<span className="text-typebox-correct inline">{promptTyped}</span>
-						<span className="text-red-600 inline">{promptTypedWrong}</span>
-						{/* <span className="bg-typebox-completed text-typebox-typed-text inline">{promptComplete}</span>
+						<span className="text-red-600 inline font-bold ">
+							{promptTypedWrong}
+						</span>
+						{/*<span className="bg-typebox-completed text-typebox-typed-text inline">{promptComplete}</span>
 						<span className="bg-typebox-correct text-typebox-typed-text inline">{promptTyped}</span>
-						<span className="bg-typebox-incorrect text-typebox-typed-text inline">{promptTypedWrong}</span> */}
+						<span className="bg-typebox-incorrect text-typebox-typed-text inline">{promptTypedWrong}</span>*/}
 						<span className={untypedClass}>{promptUntyped}</span>
 					</div>
 					<div className="absolute top-0 opacity-0 size-xl w-100/100 h-100/100">
